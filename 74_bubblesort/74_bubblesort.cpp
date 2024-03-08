1549bba8a60dc49cd584e0ed32371e5dbdc356d4
HaniefuL <haniefulgeofarry@gmail.com>
2024-03-08 18:31:28 +0700

    menambahkan display

--- 74_bubblesort/74_bubblesort.cpp
+++ 74_bubblesort/74_bubblesort.cpp
@@ -52,4 +52,22 @@ void bubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubbl
         } while (pass <= n - 1);
     }
 
-    void 
+    void display() {
+        cout << endl;
+        cout << "=================================" << endl;
+        cout << "Element Array yang telah tersusun" << endl;
+        cout << "=================================" << endl;
+        cout << endl;
+        for (int j = 0; j < n; j++) {
+            cout << arr[j]; // menampilkan array
+            if (j < n - 1) {
+                cout << " -->";
+            }
+        }
+        cout << endl;
+        cout << endl;
+
+        cout << "Jumlah Pass = " << n - 1 << endl; // menampilkan jumlah pass
+        cout << endl;
+        cout << endl;
+    }
