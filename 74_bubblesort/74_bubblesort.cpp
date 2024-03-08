f4fa10a1bba7a10499f1e03e1e33e5e2ab9198a1
HaniefuL <haniefulgeofarry@gmail.com>
2024-03-08 18:00:22 +0700

    memasukan data

--- 74_bubblesort/74_bubblesort.cpp
+++ 74_bubblesort/74_bubblesort.cpp
@@ -2,19 +2,32 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
+int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen array
+hhh
+
+void input() {      //procedur untuk input
+
+    while (true) {
+        cout << "Masukan banyaknya elemen pada erray : "; //output ke layar
+        cin >> n;   //input dari pengguna
+        if (n <= 20) // jika n kurang dari atau sama dengan 20
+            break;   // keluar dari loop
+        else {       // juka n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // output ke layar
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++) {  // looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": "; //Output ke layar
+        cin >> arr[i];              //output dari pengguna
+
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
