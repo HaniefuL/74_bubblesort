5b010c73c8f4e2c6961ccee912b7380692bb5618
HaniefuL <haniefulgeofarry@gmail.com>
2024-03-08 18:00:46 +0700

    memeperbaiki error

--- 74_bubblesort/74_bubblesort.cpp
+++ 74_bubblesort/74_bubblesort.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
 int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen array
-hhh
 
 void input() {      //procedur untuk input
 
